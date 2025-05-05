8f5d83341bce94b06a5342657d0054a2ae784a8c
TimPulin <tigeronsun@gmail.com>
2025-05-05 14:15:05 +0300

    set swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Sequence Generator API')
+    .setDescription('Sequence Generator API documentation')
+    .setVersion('1.0')
+    .addTag('sg')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
