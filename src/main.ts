f844cfae3a6d35ae6537f0e7e003a4254b93435f
gen <kyliki@yandex.ru>
2025-05-10 12:54:02 +0300

    update twizzle v20

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 
 bootstrap();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'debug', 'error', 'warn', 'verbose'],
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   setSwagger(app);
 
-  await app.listen(process.env.PORT ?? 4000);
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port);
+
+  const logger = new Logger();
+  logger.log(`Приложение запущено. port: ${port}`);
 }
 
 function setSwagger(app: INestApplication) {
